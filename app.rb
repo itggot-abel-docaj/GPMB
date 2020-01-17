e5a552229a48b8f48e8e7b1db52ef6a193a99d1a
itggot-abel-docaj <43438034+itggot-abel-docaj@users.noreply.github.com>
2020-01-17 14:20:56 +0100

    added list framework

--- app.rb
+++ app.rb
@@ -3,10 +3,13 @@ class App < Sinatra::Base
     before do
         @db = SQLite3::Database.new('db/GPMB.db')
         @db.results_as_hash = true
+        @notes = @db.execute("SELECT * FROM note")
         @current_user = @db.execute("SELECT * FROM users WHERE id = 1").first
     end
 
     get '/home' do
+        p @notes
+        #p @current_user
         slim :'home/index'
     end
 end
